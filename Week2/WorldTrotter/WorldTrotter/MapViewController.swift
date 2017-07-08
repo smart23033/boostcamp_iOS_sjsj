b81f3ba2b3c95008d416078c33bd1dc3a811c476
김성준 <smart23033@gmail.com>
2017-07-08 15:50:32 +0900

    complete Chapter 6

--- Week2/WorldTrotter/WorldTrotter/MapViewController.swift
+++ Week2/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // 지도 뷰 생성
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(segControl:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +44,19 @@ class MapViewController: UIViewController {
         print("MapViewController loaded its view")
         
     }
+    
+    func mapTypeChanged(segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    
 }
