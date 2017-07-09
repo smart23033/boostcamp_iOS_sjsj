ef73c461da0aacb2a27c5265cbaf7c3a7be3f878
김성준 <smart23033@gmail.com>
2017-07-09 19:21:46 +0900

    pull request test

---
+++ Week2/TouchTracker/Line.swift
@@ -0,0 +1,9 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by 김성준 on 2017. 7. 8..
+//  Copyright © 2017년 김성준. All rights reserved.
+//
+
+import Foundation
---
+++ Week2/TouchTracker/TouchTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by 김성준 on 2017. 7. 8..
+//  Copyright © 2017년 김성준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
