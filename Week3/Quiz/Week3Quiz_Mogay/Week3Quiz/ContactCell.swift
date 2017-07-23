137f454768b787a8a1557e10f1c809a07c28d6ec
김성준 <smart23033@gmail.com>
2017-07-23 18:42:59 +0900

    week3 quiz mogay

---
+++ Week3/Quiz/Week3Quiz_Mogay/Week3Quiz/ContactCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContactCell.swift
+//  Week3Quiz
+//
+//  Created by 김성준 on 2017. 7. 20..
+//  Copyright © 2017년 LEEJUN. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+}
