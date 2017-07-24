d7aa1847a321258244f23a9eeabb579f4b6f4989
김성준 <smart23033@gmail.com>
2017-07-25 00:37:18 +0900

    finish chapter 17 without medal challenge

---
+++ Week4/TouchTracker/Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by 김성준 on 2017. 7. 8..
+//  Copyright © 2017년 김성준. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
---
+++ Week4/TouchTracker/TouchTracker/DrawView.swift
@@ -0,0 +1,197 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by 김성준 on 2017. 7. 8..
+//  Copyright © 2017년 김성준. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+//    var currentLine: Line?
+    var currentLines = [NSValue:Line]()
+    var finishedLines = [Line]()
+    var selectedLineIndex: Int?
+    
+    
+    @IBInspectable var finishedLineColor: UIColor = UIColor.black {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var currentLineColor: UIColor = UIColor.red {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var lineThickness: CGFloat = 10 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTap))
+        doubleTapRecognizer.delaysTouchesBegan = true
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+    }
+    
+    func indexOfLineAtPoint(_ point: CGPoint) -> Int? {
+        //point와 가까운 선을 찾는다.
+        for (index, line) in finishedLines.enumerated() {
+            let begin = line.begin
+            let end = line.end
+            
+            //선 위의 포인트를 검사한다
+            for t in stride(from: CGFloat(0), to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                // 탭한 지점이 선과 20포인트 이내라면 이 선을 반환
+                if hypot(x - point.x, y - point.y) < 20.0 {
+                    return index
+                }
+            }
+        }
+        // 탭한 지점과 가까운 선이 없다면 선을 선택하지 않는다
+        return nil
+    }
+    
+    func tap(gestureRecognizer: UITapGestureRecognizer) {
+        print("Recognized a tap")
+        
+        let point = gestureRecognizer.location(in: self)
+        selectedLineIndex = indexOfLineAtPoint(point)
+        
+        setNeedsDisplay()
+    }
+    
+    func doubleTap(gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a double tap")
+        
+        selectedLineIndex = nil
+        currentLines.removeAll(keepingCapacity: false)
+        finishedLines.removeAll(keepingCapacity: false)
+        setNeedsDisplay()
+        
+    }
+    
+    func strokeLine(line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = lineThickness
+        path.lineCapStyle = CGLineCap.round
+        
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // 완성된 선은 검은색
+        finishedLineColor.setStroke()
+        for line in finishedLines {
+            strokeLine(line: line)
+        }
+        
+//        if let line = currentLine {
+            // 현재 그리는 중인 선은 빨간색
+//            UIColor.red.setStroke()
+//            strokeLine(line: line)
+//        }
+        
+        // 현재 그리는 중인 선은 빨간색
+        currentLineColor.setStroke()
+        for (_, line) in currentLines {
+            strokeLine(line: line)
+        }
+        
+        if let index = selectedLineIndex {
+            UIColor.green.setStroke()
+            let selectedLine = finishedLines[index]
+            strokeLine(line: selectedLine)
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first!
+        // view의 좌표계에서 터치의 위치를 얻는다
+//        let location = touch.location(in: self)
+//        
+//        currentLine = Line(begin: location, end: location)
+
+        print(#function)
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            let newLine = Line(begin: location, end: location)
+            
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first!
+//        let location = touch.location(in: self)
+//        
+//        currentLine?.end = location
+
+        print(#function)
+        
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key]?.end = touch.location(in: self)
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if var line = currentLine {
+//            let touch = touches.first!
+//            let location = touch.location(in: self)
+//            line.end = location
+//            
+//            finishedLines.append(line)
+//        }
+//        currentLine = nil
+
+        print(#function)
+        
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            if var line = currentLines[key] {
+                line.end = touch.location(in: self)
+                
+                finishedLines.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        print(#function)
+        
+        currentLines.removeAll()
+        
+        setNeedsDisplay()
+    }
+}
