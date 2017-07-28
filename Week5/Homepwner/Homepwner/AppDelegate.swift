00415a5ffc6a58f8dbb3d03982978e31281c1086
김성준 <smart23033@gmail.com>
2017-07-28 15:49:45 +0900

    finish chapter 15. archiving

--- Week5/Homepwner/Homepwner/AppDelegate.swift
+++ Week5/Homepwner/Homepwner/AppDelegate.swift
@@ -12,12 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let itemStore = ItemStore()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let itemStore = ItemStore()
         let imageStore = ImageStore()
         
 //        let itemsController = window!.rootViewController as! ItemViewController
@@ -34,8 +34,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let success = itemStore.saveChanges()
+        if success {
+            print("saved all of the items")
+        }
+        else {
+            print("could not save any of the items")
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
